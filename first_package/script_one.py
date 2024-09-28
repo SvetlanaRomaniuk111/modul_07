eb010f7f715fb0b67d10279b6061e583405e7c34
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2024-09-28 18:24:52 +0300

    commit to GIT

---
+++ first_package/script_one.py
@@ -0,0 +1,10 @@
+import math
+
+def get_square(num):
+    result = math.pow(num, 2)
+
+    print(f'Square of a number {num} = {result}')
+
+if __name__ == '__mane__':
+    get_square(5)
+
---
+++ second_package/script_two.py
@@ -0,0 +1,4 @@
+from first_package.script_one import get_square
+
+get_square(4)
+
